6fbf2b7b19c81f27666aa48187206881d2eb5a25
Jitze van der Hoek <99055620@mydavinci.nl>
2019-09-11 09:22:39 +0200

    compleet pizza rekenmachine

---
+++ b1w2l1 kopie.js
@@ -0,0 +1,36 @@
+alert('welkom onze site');
+
+
+
+
+var small = prompt ("voer hier de hoeveelheid small pizza's van €5,-")
+document.write("small pizza's(20 cm):"); document.write(small); 
+document.write("<br   />"); 
+var medium = prompt ("voer hier de hoeveelheid small pizza's van €7,50,-")
+document.write("medium pizza's(25 cm):"); document.write(medium); 
+document.write("<br   />"); 
+var large = prompt ("voer hier de hoeveelheid large pizza's van €10,-") 
+document.write("large pizza's(30 cm):"); document.write(large); 
+document.write("<br   />"); 
+
+
+
+
+var pizzas = 5
+var pizzam = 7.50
+var pizzal = 10
+document.write ('--------------------------------------------' )
+document.write ('<br/>' )
+document.write ('totaal: €' )
+document.write ((pizzas * parseInt (small) +  pizzam * parseInt(medium) + pizzal * parseInt(large))  ) 
+
+
+
+Jitze van der Hoek
+Pizzacalculator
+
+
+
+
+
+
