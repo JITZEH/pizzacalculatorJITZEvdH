b4af6ea13308ca7aeed651ec275181351e8b33ad
Jitze van der Hoek <99055620@mydavinci.nl>
2019-09-11 09:31:04 +0200

    4

--- b1w2l1 kopie.js
+++ b1w2l1 kopie.js
@@ -1,28 +1,28 @@
-alert('welkom onze site');
+alert('welkom onze site'); // = een melding
 
 
 
 
-var small = prompt ("voer hier de hoeveelheid small pizza's van €5,-")
-document.write("small pizza's(20 cm):"); document.write(small); 
-document.write("<br   />"); 
-var medium = prompt ("voer hier de hoeveelheid small pizza's van €7,50,-")
-document.write("medium pizza's(25 cm):"); document.write(medium); 
-document.write("<br   />"); 
-var large = prompt ("voer hier de hoeveelheid large pizza's van €10,-") 
-document.write("large pizza's(30 cm):"); document.write(large); 
-document.write("<br   />"); 
+var small = prompt ("voer hier de hoeveelheid small pizza's van €5,-") // melding vwaar je kan invooeren hoevel small pizzas
+document.write("small pizza's(20 cm):"); document.write(small); //hij schrijf woorden en hoeveelheid pizza's
+document.write("<br   />"); //een enter
+var medium = prompt ("voer hier de hoeveelheid small pizza's van €7,50,-") // melding vwaar je kan invooeren hoevel meduim pizzas
+document.write("medium pizza's(25 cm):"); document.write(medium); //hij schrijf woorden en hoeveelheid pizza's
+document.write("<br   />"); //een enter
+var large = prompt ("voer hier de hoeveelheid large pizza's van €10,-") // melding vwaar je kan invooeren hoevel large pizzas
+document.write("large pizza's(30 cm):"); document.write(large); //hij schrijf woordenen hoeveelheid pizza's
+document.write("<br   />"); //een enter
 
 
 
 
-var pizzas = 5
-var pizzam = 7.50
-var pizzal = 10
-document.write ('--------------------------------------------' )
-document.write ('<br/>' )
-document.write ('totaal: €' )
-document.write ((pizzas * parseInt (small) +  pizzam * parseInt(medium) + pizzal * parseInt(large))  ) 
+var pizzas = 5     //de kosten van de pizza koppelen
+var pizzam = 7.50  //de kosten van de pizza koppelen
+var pizzal = 10    //de kosten van de pizza koppelen
+document.write ('--------------------------------------------' ) //een streep
+document.write ('<br/>' ) //een enter
+document.write ('totaal: €' ) //hij schrijf woorden
+document.write ((pizzas * parseInt (small) +  pizzam * parseInt(medium) + pizzal * parseInt(large))  ) //berekening van het totaal bedrag
 
 
 
